81546c5b3a1a4eda3685874f54c5a690cf11706e
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2022-08-25 18:52:12 +0300

    feature: work slider for desktop

--- pages/index.js
+++ pages/index.js
@@ -16,3 +16,29 @@ switchThemePage.addEventListener('click', () => {
     content.classList.remove('page_theme_dark');
     }
 });
+//////////////////////////////////////////////////////////////////
+const buttonPrev = document.querySelector('#buttonPrev');
+const buttonNext = document.querySelector('#buttonNext');
+const sliderItems = document.querySelectorAll('.slider__item');
+let index = 0;
+buttonNext.addEventListener('click', nextSlide);
+buttonPrev.addEventListener('click', prevSlide);
+
+function nextSlide() {
+  index === sliderItems.length - 1 ? index = 0 : index++;
+  activeSlide(index);
+}
+
+function prevSlide() {
+  index === 0 ? index = sliderItems.length - 1 : index--;
+  activeSlide(index);
+}
+
+function activeSlide(n) {
+  for (item of sliderItems) {
+    item.classList.remove('slider__item_active');
+  }
+  sliderItems[n].classList.add('slider__item_active');
+}
+
+
