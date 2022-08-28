8993cbd536917c836cca6bf740e5fc42baba3892
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2022-08-28 22:26:57 +0300

    feature: added footer for desktop

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,22 @@ const content = document.querySelector('.page');
 const header = content.querySelector('.header');
 const menuButton = header.querySelector('.button_type_menu');
 const menu = header.querySelector('.header__menu');
-const switchThemePage = header.querySelector('.switch-theme');
-const selectorThemePage = switchThemePage.querySelector('.switch-theme__invisible-checkbox');
+const themePageSwitches = content.querySelectorAll('.switch-theme');
+//const selectorThemePage = switchThemePage.querySelector('.switch-theme__invisible-checkbox');
 
 menuButton.addEventListener('click', () => {
   menuButton.classList.toggle('button_type_close');
   menu.classList.toggle('header__menu_opened');
 });
-switchThemePage.addEventListener('click', () => {
-  if (selectorThemePage.checked) {
-    content.classList.add('page_theme_dark');
+themePageSwitches.forEach((themePageSwitch) => {
+  themePageSwitch.addEventListener('click', () => {
+    const selectorThemePage = themePageSwitch.querySelector('.switch-theme__invisible-checkbox');
+    if (selectorThemePage.checked) {
+      content.classList.add('page_theme_dark');
     } else {
-    content.classList.remove('page_theme_dark');
+      content.classList.remove('page_theme_dark');
     }
+  });
 });
 //////////////////////////////////////////////////////////////////
 const buttonPrev = document.querySelector('#buttonPrev');
