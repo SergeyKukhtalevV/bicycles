a85b03dffcb9106890e4358f3f9c989fa4ea1c04
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2022-09-02 00:36:31 +0300

    fix: draw in section bicycles dots

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,30 @@ const descriptionSections = content.querySelectorAll('.section__description');
 const sliderButtons = content.querySelectorAll('.slider__button');
 const sliderButtonImages = content.querySelectorAll('.slider__button-image');
 //const selectorThemePage = switchThemePage.querySelector('.switch-theme__invisible-checkbox');
+const containerRadioButtons = content.querySelector('.bicycles__radio-buttons');
+const invisibleRadioButtons = content.querySelectorAll('.switch-bicycles__invisible-radio');
+//const switchBicycles = content.querySelectorAll('.switch-bicycles__invisible-radio');
+const galleryBicycles = content.querySelectorAll('.bicycles__gallery');
+
+const dotsBicycles = content.querySelector('.bicycles__dots');
+const dotTemplate = document.querySelector('#item-template').content;
+
+
+const optionSelectBicycle = content.querySelector('.bicycles__select-option');
+let expanded = false;
+invisibleRadioButtons.forEach((item) => {
+  if(item.hasAttribute('checked')) {
+    optionSelectBicycle.textContent = item.nextElementSibling.textContent;
+  }
+});
 drawDots();
+drawActiveDot();
+//Функция открытия и закрытия меню на смартфоне
 menuButton.addEventListener('click', () => {
   menuButton.classList.toggle('button_type_close');
   nav.classList.toggle('header__nav_opened');
 });
+//Функция переключения темы сайта
 themePageSwitches.forEach((themePageSwitch) => {
   themePageSwitch.addEventListener('click', () => {
     const selectorThemePage = themePageSwitch.querySelector('.switch-theme__invisible-checkbox');
@@ -73,37 +92,39 @@ function activeSlide(n) {
   sliderItems[n].classList.add('slider__item_active');
 }
 /////////////////////////////////////////////////////////
-const switchBicycles = content.querySelectorAll('.switch-bicycles__invisible-radio');
-const galleryBicycles = content.querySelectorAll('.bicycles__gallery');
 
-switchBicycles.forEach((switchBicycle, index) => {
+invisibleRadioButtons.forEach((switchBicycle, index) => {
   switchBicycle.addEventListener('click', () =>{
+    showRadioButtons();
+    deleteDots();
+    optionSelectBicycle.textContent = switchBicycle.nextElementSibling.textContent;
     galleryBicycles.forEach((galleryBicycle, key) =>{
-      index = (index > 2) ? index - 3 : index;
-      console.log(index, key);
+      index = (index > 2) ? index - invisibleRadioButtons.length / 2 : index;
       if(key === index) {
+        console.log(optionSelectBicycle.textContent);
         galleryBicycle.classList.add('bicycles__gallery_active');
         drawDots();
+        drawActiveDot();
       } else {
-        deleteDots();
         galleryBicycle.classList.remove('bicycles__gallery_active');
       }
     });
   });
 });
 /////////////////////////////////////////////////////////
+function createDot() {
+  const dotElement = dotTemplate.querySelector('.bicycles__dot').cloneNode(true);
+  dotsBicycles.append(dotElement);
+}
 function drawDots() {
+
   const activeGalleryBicycles = content.querySelector('.bicycles__gallery_active');
   const itemBicycles = activeGalleryBicycles.querySelectorAll('.bicycles__item');
   const cntItemBicycles = itemBicycles.length;
-  const dotsBicycles = content.querySelector('.bicycles__dots');
-  const dotTemplate = document.querySelector('#item-template').content;
-  const dotElement = dotTemplate.querySelector('.bicycles__dot').cloneNode(true);
 
-  console.log(activeGalleryBicycles);
+  //console.log(dotElement);
   for (let i = 0; i < cntItemBicycles; i++) {
-    dotsBicycles.prepend(dotElement);
-    console.log(dotsBicycles);
+    createDot();
   }
 }
 function deleteDots() {
@@ -112,16 +133,26 @@ function deleteDots() {
     dot.remove();
   })
 }
+function drawActiveDot() {
+
+  const dotsBicycles = content.querySelectorAll('.bicycles__dot');
+  const activeGalleryBicycles = content.querySelector('.bicycles__gallery_active');
+  const itemsBicycles = activeGalleryBicycles.querySelectorAll('.bicycles__item');
+  itemsBicycles.forEach((item, n) => {
+    if(item.classList.contains('bicycles__item_active')) {
+      dotsBicycles[n].classList.add('bicycles__dot_active');
+    }
+  });
+}
 ////////////////////////////////////////////////////////////////////
-let expanded = false;
 
 function showRadioButtons() {
-  let checkboxes = document.querySelector('.bicycles__radio-buttons');
+
   if (!expanded) {
-    checkboxes.style.display = "block";
+    containerRadioButtons.style.display = "block";
     expanded = true;
   } else {
-    checkboxes.style.display = "none";
+    containerRadioButtons.style.display = "none";
     expanded = false;
   }
 }
